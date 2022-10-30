fda0e16d75bbafccd9c283ef205039384009897c
nitwhiz <schonfeldandy@gmail.com>
2022-10-30 13:44:17 +0100

    configure esbuild for production

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,17 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import eslintPlugin from 'vite-plugin-eslint';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [vue(), eslintPlugin()],
   server: {
     host: '0.0.0.0',
     hmr: false,
   },
-});
+  esbuild:
+    mode === 'production'
+      ? {
+          drop: ['console', 'debugger'],
+          platform: 'browser',
+        }
+      : {},
+}));
